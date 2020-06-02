06cfc0a74b3170eacbcb445726f7b96403ea762c
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-06-02 17:19:12 +1000

    Add support for assignment notifications

--- src/assignment.rs
+++ src/assignment.rs
@@ -3,6 +3,7 @@ use std::fmt;
 pub(crate) struct Assignment {
     name: String,
     date: chrono::NaiveDate,
+    notification: url::Url,
 }
 
 impl Assignment {
@@ -14,11 +15,19 @@ impl Assignment {
 
         let (s, _) = crate::take_whitespace1(s)?;
 
+        let (s, url) = crate::take_until_whitespace(s)?;
+
+        let url =
+            url::Url::parse(url).map_err(|_| nom::Err::Error((s, nom::error::ErrorKind::Tag)))?;
+
+        let (s, _) = crate::take_whitespace1(s)?;
+
         Ok((
             "",
             Self {
                 name: s.to_string(),
                 date,
+                notification: url,
             },
         ))
     }
@@ -28,9 +37,10 @@ impl fmt::Display for Assignment {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "‘{}’, which is due on {}",
+            "‘{}’, which is due on {}. Notification: {}",
             self.name,
-            self.date.format("%d %B, %Y")
+            self.date.format("%d %B, %Y"),
+            self.notification
         )
     }
 }
